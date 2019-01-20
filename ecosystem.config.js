27fa37421c45c75d2ca57c32b74283533a9ab0e6
Semyon Fomin <taxnuke@gmail.com>
2019-01-21 01:11:36 +0300

    Fix repo url

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       user: 'pi',
       host: '10.0.1.26',
       ref: 'origin/master',
-      repo: 'git@github.com:taxnuke/raspberry-pi-widget.git',
+      repo: 'https://github.com/taxnuke/raspberry-pi-widget.git',
       path: '/home/pi/rpi-widget',
       'post-deploy': 'npm i && pm2 reload ecosystem.config.js'
     }
